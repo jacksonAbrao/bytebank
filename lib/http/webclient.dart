5ff9b7ec2078f25f70a0d0603ceded801bed3bd3
jackson.comp1681@hotmail.com <jackson.comp1681@hotmail.com>
2022-04-26 10:58:02 -0300

    alt

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -31,12 +31,15 @@ class LoggingInterceptor implements InterceptorContract {
   }
 }
 
+final Client client = InterceptedClient.build(
+  interceptors: [LoggingInterceptor()],
+);
+
+Uri baseUrl = Uri.parse(("http://10.1.1.102:8080/transactions"));
+
 Future<List<Transaction>> findAll() async {
-  Client client = InterceptedClient.build(
-    interceptors: [LoggingInterceptor()],
-  );
-  Uri url = Uri.parse(("http://10.1.1.102:8080/transactions"));
-  final Response response = await client.get(url);
+  final Response response =
+      await client.get(baseUrl).timeout(const Duration(seconds: 5));
   final List<dynamic> decodedJson = jsonDecode(response.body);
   final List<Transaction> transactions = [];
   for (Map<String, dynamic> transactionJson in decodedJson) {
@@ -52,4 +55,35 @@ Future<List<Transaction>> findAll() async {
     transactions.add(transaction);
   }
   return transactions;
+}
+
+Future<Transaction> save(Transaction transaction) async {
+  final Map<String, dynamic> transactionMap = {
+    'value': transaction.value,
+    'contact': {
+      'name': transaction.contact.name,
+      'acconuntNumber': transaction.contact.accountNumber
+    }
+  };
+
+  final String transactionJson = jsonEncode(transactionMap);
+
+  final Response response = await client.post(baseUrl,
+      headers: {
+        'Content-type': 'application/json',
+        'password': '1000',
+      },
+      body: transactionJson);
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = json['contact'];
+  // ignore: dead_code
+  return Transaction(
+    json['value'],
+    Contact(
+      0,
+      contactJson['name'],
+      contactJson['accountNumber'],
+    ),
+  );
 }
--- lib/screens/transactions_list.dart
+++ lib/screens/transactions_list.dart
@@ -1,4 +1,5 @@
 import 'package:bytebank/http/webclient.dart';
+import 'package:bytebank/screens/centered_message.dart';
 import 'package:flutter/material.dart';
 import '../components/progress.dart';
 import '../models/transactions.dart';
@@ -24,34 +25,41 @@ class TransactionsList extends StatelessWidget {
             case ConnectionState.active:
               break;
             case ConnectionState.done:
-              final List<Transaction>? transactions = snapshot.data;
-              return ListView.builder(
-                itemBuilder: (context, index) {
-                  final Transaction transaction = transactions![index];
-                  return Card(
-                    child: ListTile(
-                      leading: const Icon(Icons.monetization_on),
-                      title: Text(
-                        transaction.value.toString(),
-                        style: const TextStyle(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
+              if (snapshot.hasData) {
+                final List<Transaction>? transactions = snapshot.data;
+                if (transactions!.isNotEmpty) {
+                  return ListView.builder(
+                    itemBuilder: (context, index) {
+                      final Transaction transaction = transactions[index];
+                      return Card(
+                        child: ListTile(
+                          leading: const Icon(Icons.monetization_on),
+                          title: Text(
+                            transaction.value.toString(),
+                            style: const TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                            transaction.contact.accountNumber.toString(),
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                            ),
+                          ),
                         ),
-                      ),
-                      subtitle: Text(
-                        transaction.contact.accountNumber.toString(),
-                        style: const TextStyle(
-                          fontSize: 16.0,
-                        ),
-                      ),
-                    ),
+                      );
+                    },
+                    itemCount: transactions.length,
                   );
-                },
-                itemCount: transactions?.length,
+                }
+              }
+              return CenteredMessage(
+                'No Transactions found',
+                icon: Icons.warning,
               );
           }
-
-          return Text("Unknow error");
+          return CenteredMessage("Unknow error");
         },
       ),
     );
